3173fd705e341c944eb68b51beced158d9eebfa1
blair488 <samantha99fan@gmail.com>
2021-04-07 17:01:25 +0800

    update main.cpp

--- Exam_1/main.cpp
+++ Exam_1/main.cpp
@@ -4,13 +4,10 @@
 using namespace std::chrono;
 
 uLCD_4DGL uLCD(D1, D0, D2);
-//DigitalIn up_button(D4);
-//DigitalIn down_button(D5);
-//DigitalIn confirm_button(D6);
 InterruptIn up_button(D4);
 InterruptIn down_button(D5);
 InterruptIn confirm_button(D6);
-DigitalIn mypin(USER_BUTTON);
+InterruptIn mypin(USER_BUTTON);
 
 AnalogOut Aout(D7);
 AnalogIn Ain(A0);
@@ -28,6 +25,31 @@ int sample_rate = 400;
 EventQueue G_Queue;
 EventQueue S_Queue;
 
+void display_freq(){
+    if(freq_change || freq_set){
+        uLCD.cls();
+        uLCD.text_width(2);
+        uLCD.text_height(2);
+        uLCD.textbackground_color(BLACK);
+        uLCD.color(WHITE);
+        uLCD.locate(2,1);
+        uLCD.printf("Menu");
+        for(int i=0; i<4; i++){
+            if(freq_change && choose_freq==i) uLCD.textbackground_color(0x00CC66);
+            else if(freq_set && set_freq==i) uLCD.textbackground_color(0x3333FF);
+            else uLCD.textbackground_color(BLACK);
+            uLCD.locate(2,2+i);
+            if(i==0) uLCD.printf("1");
+            else if(i==1) uLCD.printf("1/2");
+            else if(i==2) uLCD.printf("1/4");
+            else if(i==3) uLCD.printf("1/8");
+            uLCD.printf("rate");
+        }
+        freq_change = 0;
+        freq_set = 0;
+    }
+}
+
 void flip_up()
 {
    if(choose_freq>0){
@@ -44,19 +66,6 @@ void flip_down()
         }
         freq_change = 1;
     display_freq();
-}
-
-void selection_done()
-{
-    set_freq = choose_freq;
-    if(choose_freq==0) frequency = 1;
-    else if(choose_freq==1) frequency = 2;
-    else if(choose_freq==2) frequency = 4;
-    else if(choose_freq==3) frequency = 8;
-    freq_set = 1;
-    display_freq();
-    
-    G_Queue.call(generation);
 }
 
 void generation()
@@ -143,9 +152,17 @@ void generation()
         }
 }
 
-void press_userbutton()
+void selection_done()
 {
-    S_Queue.call(sampling);
+    set_freq = choose_freq;
+    if(choose_freq==0) frequency = 1;
+    else if(choose_freq==1) frequency = 2;
+    else if(choose_freq==2) frequency = 4;
+    else if(choose_freq==3) frequency = 8;
+    freq_set = 1;
+    display_freq();
+    
+    G_Queue.call(&generation);
 }
 
 void sampling()
@@ -156,29 +173,9 @@ void sampling()
     }
 }
 
-void display_freq(){
-    if(freq_change || freq_set){
-        uLCD.cls();
-        uLCD.text_width(2);
-        uLCD.text_height(2);
-        uLCD.textbackground_color(BLACK);
-        uLCD.color(WHITE);
-        uLCD.locate(2,1);
-        uLCD.printf("Menu");
-        for(int i=0; i<4; i++){
-            if(freq_change && choose_freq==i) uLCD.textbackground_color(0x00CC66);
-            else if(freq_set && set_freq==i) uLCD.textbackground_color(0x3333FF);
-            else uLCD.textbackground_color(BLACK);
-            uLCD.locate(2,2+i);
-            if(i==0) uLCD.printf("1");
-            else if(i==1) uLCD.printf("1/2");
-            else if(i==2) uLCD.printf("1/4");
-            else if(i==3) uLCD.printf("1/8");
-            uLCD.printf("rate");
-        }
-        freq_change = 0;
-        freq_set = 0;
-    }
+void press_userbutton()
+{
+    S_Queue.call(&sampling);
 }
 
 int main()
